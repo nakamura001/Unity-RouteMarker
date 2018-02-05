c4537f1e89fe2ec630271118a2f37e5058b72daa
takashi nakamura <polluxtk@gmail.com>
2018-02-05 18:53:11 +0900

    Initial commit.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+    float speedPower = 10.0f;
+    float jumpPower = 30.0f;
+    float gravityPower = 10.0f;
+    float inAirVelocity = 0f;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var motion = transform.transform.right * Input.GetAxis("Horizontal");
+        motion *= speedPower;
+
+        if (controller.isGrounded && Input.GetButtonDown("Jump")) {
+            inAirVelocity = jumpPower;
+        }
+        motion.y = inAirVelocity - gravityPower;
+        controller.Move(motion * Time.deltaTime);
+
+        inAirVelocity -= gravityPower * .2f;
+        if (inAirVelocity < 0)
+        {
+            inAirVelocity = 0;
+        }
+}
+}
